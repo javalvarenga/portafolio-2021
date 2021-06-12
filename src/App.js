b7793e3ebf7e2a61c3981e56ef0e7715c677c116
javalvarenga <abraalvare@gmail.com>
2021-06-12 12:34:19 -0600

    add route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/portafolio-2021" component={Home} />
       <Route exact path="/cursos" component={Cursos} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/about" component={About} />
