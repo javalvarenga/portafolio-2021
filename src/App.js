aebd2d9a6fd05b08fe8a9ef076e743f0be23bd93
javalvarenga <abraalvare@gmail.com>
2021-06-12 14:02:32 -0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import Cursos from './pages/Cursos';
 import About from './pages/About';
 import Projects from './pages/Projects';
 function App() {
   return (
-    <BrowserRouter>
-    <Switch>
+    <HashRouter>
+
       <Route exact path="/" component={Home} />
       <Route exact path="/portafolio-2021" component={Home} />
-      <Route exact path="/portafolio-2021/cursos" component={Cursos} />
-      <Route exact path="/portafolio-2021/projects" component={Projects} />
-      <Route exact path="/portafolio-2021/about" component={About} />
-    </Switch>
-    </BrowserRouter>
+      <Route exact path="/cursos" component={Cursos} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/about" component={About} />
+
+    </HashRouter>
   );
 }
 
