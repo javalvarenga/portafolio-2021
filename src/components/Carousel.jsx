e00fbc9c3aa22c4072a4545f034c923a177f3267
javalvarenga <abraalvare@gmail.com>
2021-07-15 19:50:40 -0600

    project section carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Carousel.css";
+
+function Carousel() {
+  return (
+    <div className="Carousel">
+      <div className="imagesContainer"></div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+
+import "./styles/Projects.css";
+import Carousel from "../components/Carousel";
 
 function Projects() {
-    return (
-        <div>
-            
+  return (
+    <div className="Projects">
+      <section>
+        <div className="project">
+          <div className="project_description">
+            <h2>Platzi-tiendita</h2>
+            <h5>
+              e-commerce<i></i> || <span>Javascript/React</span>
+            </h5>
+          </div>
+          <hr />
+
+          <div className="project-carousel">
+            <Carousel />
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
