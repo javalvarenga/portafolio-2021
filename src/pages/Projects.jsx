e834f8dd865f58763edef340d050d7aa983d4831
javalvarenga <abraalvare@gmail.com>
2021-07-19 14:19:57 -0600

    first project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import "./styles/Projects.css";
-import Carousel from "../components/Carousel";
+import Carousel from "../components/CarouselFn";
+import demo from "../assets/images/global.svg";
+import github from "../assets/images/github.svg";
 
 function Projects() {
   return (
@@ -11,16 +13,33 @@ function Projects() {
           <div className="project_description">
             <h2>Platzi-tiendita</h2>
             <h5>
-              e-commerce<i></i> || <span>Javascript/React</span>
+              <i>e-commerce</i>
             </h5>
           </div>
           <hr />
 
           <div className="project-carousel">
-            <Carousel />
+            <div className="carousel">
+              <Carousel />
+            </div>
+            <aside>
+              <div className="icons">
+              <a href="https://github.com/javalvarenga/platzi-tiendita" target="_blank" rel="">
+              <div className="icon">
+                  <img src={github} alt="github" />
+                  <p>repositorio</p>
+                </div>
+              </a>
+                <a href="https://platzi-tiendita.firebaseapp.com/" target="_blank" rel="noopener noreferrer">
+                <div className="icon">
+                  <img src={demo} alt="demo" />
+                  <p>ver demo</p>
+                </div>
+                </a>
+              </div>
+            </aside>
           </div>
         </div>
-        <h4>Seccion en desarrollo</h4>
       </section>
     </div>
   );
